e5d1e4422daf9b9c77bc5cd5b3775ef54bfe12ee
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-01-16 13:09:18 +0100

    update documentation for persephone (class)

--- R/persephone.R
+++ R/persephone.R
@@ -2,15 +2,26 @@
 #'
 #' Common interface for x13 and tramoseats
 #'
+#' @section Constructor:
+#'
+#' - `new()`: Initializes an object. See the documentation for the derived classes for details.
+#'
 #' @section Methods:
 #'
-#' - `run()`: Runs the model
+#' - `run(verbose = FALSE)`: Runs the model
 #' - `updateParams(...)`: Updates the current parameters
 #'
 #' @section Read-Only fields:
-#' * params
-#' * ts
-#' * output
+#'
+#' - `params`: A parameters object of class `SA_spec`. See [x13_spec] and [tramoseats_spec].
+#' - `ts`: The time series.
+#' - `output`: The return value from the underlying functions `x13` or `tramoseats`. This slot will
+#'   be empty (`NULL`) before `run()` is called for the first time.
+#'
+#' @section Parameters:
+#' - `verbose`: should the output from the underlying RJDemetra function be displayed after the
+#'   run? If `FALSE` (the default), the output will be returned invisibly.
+#' @seealso [x13Single], [tramoseatsSingle]
 #' @name persephone
 #' @import RJDemetra
 NULL
@@ -21,7 +32,7 @@ persephone <- R6::R6Class(
     initialize = function() {
       stop("implement this function")
     },
-    run = function() {
+    run = function(verbose = FALSE) {
       stop("implement this function")
     },
     updateParams = function(...) {
