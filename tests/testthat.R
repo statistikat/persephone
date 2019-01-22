e59de726127e6eb6cd3b90bbea514fa3fd5b6d97
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-01-22 12:23:12 +0100

    minimal unit-testing: plots

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(persephone)
+
+test_check("persephone")
---
+++ tests/testthat/test-plot.R
@@ -0,0 +1,17 @@
+context("test-plot")
+
+test_that("plotting works", {
+  data(AirPassengers, package = "datasets")
+  obj <- x13Single$new(AirPassengers, "RSA1")
+  expect_error(plot(obj, drawPoints = TRUE), NA)
+
+  obj$run()
+  expect_error(plot(obj, drawPoints = TRUE), NA)
+
+  obj$updateParams(
+    usrdef.outliersEnabled = TRUE,
+    usrdef.outliersType = c("AO", "LS", "LS"),
+    usrdef.outliersDate = c("1950-01-01","1955-04-01", "1959-10-01"))
+  obj$run()
+  expect_error(plot(obj), NA)
+})
