ae3b5a48174b7242d4ec49583be5bd939f0fd77a
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-01-23 15:57:28 +0100

    add tramoseats test

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -11,7 +11,15 @@ test_that("plotting works", {
   obj$updateParams(
     usrdef.outliersEnabled = TRUE,
     usrdef.outliersType = c("AO", "LS", "LS"),
-    usrdef.outliersDate = c("1950-01-01","1955-04-01", "1959-10-01"))
+    usrdef.outliersDate = c("1950-01-01", "1955-04-01", "1959-10-01"))
   obj$run()
   expect_error(plot(obj), NA)
+
+  ## tramoseats ----
+
+  obj <- tramoseatsSingle$new(AirPassengers)
+  expect_error(plot(obj, drawPoints = TRUE), NA)
+
+  obj$run()
+  expect_error(plot(obj, drawPoints = TRUE), NA)
 })
