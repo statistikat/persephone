f207ab3afb77f347cc81f0553098be293fff5050
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-08-22 10:44:19 +0200

    update hts documentation

--- R/hierarchicalTimeSeries.R
+++ R/hierarchicalTimeSeries.R
@@ -30,32 +30,44 @@
 #' ht2$adjusted
 #' ht2$adjusted_indirect
 #'
-#' data(ipi_c_eu,package = "RJDemetra")
+#' #-------- example with industrial price indices -----------
+#'
+#' data(ipi_c_eu, package = "RJDemetra")
 #' # Reducing the data set to the EU28 countries
-#' ipi_eu <- ipi_c_eu[,-c(1:3,32:37)]
+#' ipi_eu <- ipi_c_eu[, -c(1:3, 32:37)]
 #' # We now build persephone objects for the countries
 #' # (the lowest level in the hierachy)
 #' ts_28 <- lapply(ipi_eu, per_tramo)
+#'
 #' # We want to add an extra layer and split the EU28 countries in two groups
-
-#' ht_half_europe_1 <- do.call(per_hts,ts_28[1:14],)
-#' # possible future syntax
-#' #per_hts(ts_28[1:14], model=list(AT="x13",BE="tramoseats"),spec=list(),
-#' #outlier_enabled(..=TRUE),tradings.leapyw=list(LV=TRUE,AT=dfdjkl,))
-#' ht_half_europe_2 <- do.call(per_hts,ts_28[15:28])
+#' ht_half_europe_1 <- do.call(per_hts, ts_28[1:14])
+#'
+#' ## possible future syntax
+#' #per_hts(ts_28[1:14], model = list(AT = "x13", BE = "tramoseats"),
+#' #        spec = list(), outlier_enabled(.. = TRUE),
+#' #        tradings.leapyw = list(LV = TRUE, AT = dfdjkl, ))
+#'
+#' ht_half_europe_2 <- do.call(per_hts, ts_28[15:28])
+#'
 #' # Now we generate the object for EU28
-#'  ht_europe <- per_hts(
+#' ht_europe <- per_hts(
 #'   halfEU_1 = ht_half_europe_1,
 #'   halfEU_2 = ht_half_europe_2,
-#'   spec="RSA5c",model="x13")
+#'   spec = "RSA5c",
+#'   model = "x13"
+#' )
+#'
 #' # start the seasonal adjustment
 #' ht_europe$run()
+#'
 #' # accessing the directly and indirectly adjusted series for EU28
 #' ht_europe$adjusted
 #' ht_europe$adjusted_indirect
+#'
 #' # accessing the directly and indirectly adjusted series for half of Europe
 #' ht_europe$components$halfEU_2$adjusted
 #' ht_europe$components$halfEU_2$adjusted_indirect
+#'
 #' # accessing the adjusted series for a country
 #' ht_europe$components$halfEU_2$components$AT$adjusted
 #' @export
