d53040fe374754725b6851b41ca66ec969360f0f
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-09-02 18:13:00 +0200

    export iterate
    export a function `iterate` which wraps the
    method of `per_hts()` with the same name.
    
    TODO: link from the per_hts() documentation
          to `iterate.Rd`

--- R/as_table_nested_list.R
+++ R/as_table_nested_list.R
@@ -40,3 +40,27 @@ unnest_nested_list <- function(x, prefix = "") {
     ) %>%
     do.call(c, .)
 }
+
+#' Iterate over a hierarchical persephone object
+#'
+#' This funtion is an alias to the method `iterate` and can be used to iterate
+#' over all children of a hierarchical time series produced with `per_hts()`.
+#'
+#' @param x a hierarchical persephone object
+#' @param fun a function with one argument
+#' @param as_table should the return value be coerced to a `data.frame`?
+#' @param component a component id from which the iteration should start
+#' @param unnest should the return value be unnested, so that every component
+#'   is an entry of the resulting list?
+#'
+#' @examples
+#' obj_x13 <- per_x13(AirPassengers, "RSA3")
+#' ht <- per_hts(a = obj_x13, b = obj_x13, method = "x13")
+#' ht2 <- per_hts(a = ht, b = obj_x13)
+#' ht2$iterate(function(x) {class(x)})
+#' ht2$iterate(function(x) {list(class = class(x))}, as_table = TRUE)
+#' ht2$iterate(function(x) {list(class = class(x))}, unnest = TRUE)
+#' @export
+iterate <- function(x, fun, as_table = FALSE, component = "", unnest = FALSE) {
+ x$iterate(fun, as_table = FALSE, component = "", unnest = FALSE)
+}
