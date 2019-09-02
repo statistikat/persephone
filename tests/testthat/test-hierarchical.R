c71a4ccaa6495b32ea2102f768b61b1cd77534a0
alexkowa <alex@kowarik.net>
2019-09-02 13:48:45 +0200

    test for hts with weights

--- tests/testthat/test-hierarchical.R
+++ tests/testthat/test-hierarchical.R
@@ -4,12 +4,12 @@ test_that("adjusted", {
   obj_x13 <- per_x13(AirPassengers, "RSA3")
 
   ht <- per_hts(a = obj_x13, b = obj_x13)
-  expect_true(is.null(ht$adjusted))
+  expect_true(is.null(ht$adjusted_direct))
   expect_true(is.null(ht$adjusted_indirect))
   ht$run()
-  expect_s3_class(ht$adjusted, "ts")
+  expect_s3_class(ht$adjusted_direct, "ts")
   expect_s3_class(ht$adjusted_indirect, "ts")
-  expect_identical(tsp(ht$adjusted), tsp(ht$adjusted_indirect))
+  expect_identical(tsp(ht$adjusted_direct), tsp(ht$adjusted_indirect))
 })
 
 context("nested hierarchies")
@@ -19,9 +19,9 @@ test_that("nested hierarchies", {
   ht <- per_hts(a = obj_x13, b = obj_x13)
   ht2 <- per_hts(a = ht, b = obj_x13)
   ht2$run()
-  expect_s3_class(ht2$adjusted, "ts")
+  expect_s3_class(ht2$adjusted_direct, "ts")
   expect_s3_class(ht2$adjusted_indirect, "ts")
-  expect_identical(tsp(ht2$adjusted), tsp(ht2$adjusted_indirect))
+  expect_identical(tsp(ht2$adjusted_direct), tsp(ht2$adjusted_indirect))
 })
 
 context("incompatible time instances")
@@ -39,9 +39,9 @@ context("object run by reference")
 test_that("object run by reference", {
   obj_x13 <- per_x13(AirPassengers, "RSA3")
   ht <- per_hts(a = obj_x13)
-  expect_true(is.null(obj_x13$adjusted))
+  expect_true(is.null(obj_x13$adjusted_direct))
   ht$run()
-  expect_s3_class(obj_x13$adjusted, "ts")
+  expect_s3_class(obj_x13$adjusted_direct, "ts")
 })
 
 context("hierarchical misc")
@@ -64,4 +64,117 @@ context("hierarchical misc")
 test_that("components must be named", {
   obj_x13 <- per_x13(AirPassengers, "RSA3")
   expect_error(per_hts(obj_x13), "must be named")
+})
+
+
+context("hierachical weights")
+
+test_that("weights as scalars", {
+  obj_x13 <- per_x13(AirPassengers, "RSA3")
+  ht <- per_hts(a = obj_x13,
+                b = obj_x13,
+                c = obj_x13,
+                weights = c(2, 2, 2)
+                )
+  
+  expect_true(all.equal(ht$ts, 6 * obj_x13$ts))
+  expect_true(all(ht$weights == 2))
+  ht$run()
+  expect_true(all.equal(ht$components$a$adjusted_direct*6,ht$adjusted_indirect))
+  
+  ht2 <- per_hts(a = obj_x13,
+                 b = obj_x13,
+                 c = obj_x13,
+                 weights = c(3, 3, 3)
+  )
+  expect_true(all.equal(ht2$ts, 9 * obj_x13$ts))
+  expect_true(all(ht2$weights == 3))
+  
+  ht0 <- per_hts(a0 = ht,
+                 b0 = ht2)
+  
+  expect_true(all.equal(ht0$ts, 15 * obj_x13$ts))
+  
+  ht0$run()
+  expect_true(all.equal(ht0$adjusted_indirect,15*ht0$components$a0$components$a$adjusted_direct))
+  
+  expect_true(is.character(all.equal(ht0$adjusted_direct,15*ht0$components$a0$components$a$adjusted_direct)))
+})
+
+
+test_that("weights as mts", {
+  obj_x13 <- per_x13(AirPassengers, "RSA3")
+  w2 <- ts(2, start = start(AirPassengers),
+           end = end(AirPassengers),
+           frequency = frequency(AirPassengers))
+  w3 <- ts(3, start = start(AirPassengers),
+           end = end(AirPassengers),
+           frequency = frequency(AirPassengers))
+  ht <- per_hts(a = obj_x13,
+                b = obj_x13,
+                c = obj_x13,
+                weights = cbind(w2,w2,w2)
+  )
+  
+  expect_true(all.equal(ht$ts, 6 * obj_x13$ts))
+  expect_true(all(ht$weights == 2))
+  ht$run()
+  expect_true(all.equal(ht$components$a$adjusted_direct*6,ht$adjusted_indirect))
+  
+  ht2 <- per_hts(a = obj_x13,
+                 b = obj_x13,
+                 c = obj_x13,
+                 weights = cbind(w3,w3,w3)
+  )
+  expect_true(all.equal(ht2$ts, 9 * obj_x13$ts))
+  expect_true(all(ht2$weights == 3))
+  
+  ht0 <- per_hts(a0 = ht,
+                 b0 = ht2)
+  
+  expect_true(all.equal(ht0$ts, 15 * obj_x13$ts))
+  
+  ht0$run()
+  expect_true(all.equal(ht0$adjusted_indirect,15*ht0$components$a0$components$a$adjusted_direct))
+  
+  expect_true(is.character(all.equal(ht0$adjusted_direct,15*ht0$components$a0$components$a$adjusted_direct)))
+})
+
+
+test_that("weights as list", {
+  obj_x13 <- per_x13(AirPassengers, "RSA3")
+  w2 <- ts(2, start = start(AirPassengers),
+           end = end(AirPassengers),
+           frequency = frequency(AirPassengers))
+  w3 <- ts(3, start = start(AirPassengers),
+           end = end(AirPassengers),
+           frequency = frequency(AirPassengers))
+  ht <- per_hts(a = obj_x13,
+                b = obj_x13,
+                c = obj_x13,
+                weights = list(w2,w2,w2)
+  )
+  
+  expect_true(all.equal(ht$ts, 6 * obj_x13$ts))
+  expect_true(all(ht$weights == 2))
+  ht$run()
+  expect_true(all.equal(ht$components$a$adjusted_direct*6,ht$adjusted_indirect))
+  
+  ht2 <- per_hts(a = obj_x13,
+                 b = obj_x13,
+                 c = obj_x13,
+                 weights = list(w3,w3,w3)
+  )
+  expect_true(all.equal(ht2$ts, 9 * obj_x13$ts))
+  expect_true(all(ht2$weights == 3))
+  
+  ht0 <- per_hts(a0 = ht,
+                 b0 = ht2)
+  
+  expect_true(all.equal(ht0$ts, 15 * obj_x13$ts))
+  
+  ht0$run()
+  expect_true(all.equal(ht0$adjusted_indirect,15*ht0$components$a0$components$a$adjusted_direct))
+  
+  expect_true(is.character(all.equal(ht0$adjusted_direct,15*ht0$components$a0$components$a$adjusted_direct)))
 })
