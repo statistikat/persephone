1bfb5772d97c64c64fab56795c989ca929e60e16
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-09-03 10:38:13 +0200

    update example in iterate.R

--- R/iterate.R
+++ R/iterate.R
@@ -57,9 +57,9 @@ unnest_nested_list <- function(x, prefix = "") {
 #' obj_x13 <- per_x13(AirPassengers, "RSA3")
 #' ht <- per_hts(a = obj_x13, b = obj_x13, method = "x13")
 #' ht2 <- per_hts(a = ht, b = obj_x13)
-#' ht2$iterate(function(x) {class(x)})
-#' ht2$iterate(function(x) {list(class = class(x))}, as_table = TRUE)
-#' ht2$iterate(function(x) {list(class = class(x))}, unnest = TRUE)
+#' ht2$iterate(function(x) {class(x)[1]})
+#' ht2$iterate(function(x) {list(class = class(x)[1])}, as_table = TRUE)
+#' ht2$iterate(function(x) {list(class = class(x)[1])}, unnest = TRUE)
 #' @export
 iterate <- function(x, fun, as_table = FALSE, component = "", unnest = FALSE) {
  x$iterate(fun, as_table = FALSE, component = "", unnest = FALSE)
